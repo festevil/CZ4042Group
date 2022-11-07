bc39cef549701a29140e282007387a87aa057f5a
festevil <62298846+festevil@users.noreply.github.com>
2022-11-07 19:47:10 +0800

    Add files via upload

---
+++ transformer.py
@@ -0,0 +1,254 @@
+# taken from https://github.com/emla2805/vision-transformer
+import tensorflow as tf
+import tensorflow_addons as tfa
+from tensorflow.keras.layers import (
+    Dense,
+    Dropout,
+    LayerNormalization,
+)
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+import tensorflow_datasets as tfds
+from tensorflow import keras
+from histories_util import *
+from vit_keras import vit, utils
+from time import time
+
+
+class MultiHeadSelfAttention(tf.keras.layers.Layer):
+    def __init__(self, embed_dim, num_heads=8):
+        super(MultiHeadSelfAttention, self).__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        if embed_dim % num_heads != 0:
+            raise ValueError(
+                f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}"
+            )
+        self.projection_dim = embed_dim // num_heads
+        self.query_dense = Dense(embed_dim)
+        self.key_dense = Dense(embed_dim)
+        self.value_dense = Dense(embed_dim)
+        self.combine_heads = Dense(embed_dim)
+
+    def attention(self, query, key, value):
+        score = tf.matmul(query, key, transpose_b=True)
+        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
+        scaled_score = score / tf.math.sqrt(dim_key)
+        weights = tf.nn.softmax(scaled_score, axis=-1)
+        output = tf.matmul(weights, value)
+        return output, weights
+
+    def separate_heads(self, x, batch_size):
+        x = tf.reshape(
+            x, (batch_size, -1, self.num_heads, self.projection_dim)
+        )
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        query = self.query_dense(inputs)
+        key = self.key_dense(inputs)
+        value = self.value_dense(inputs)
+        query = self.separate_heads(query, batch_size)
+        key = self.separate_heads(key, batch_size)
+        value = self.separate_heads(value, batch_size)
+
+        attention, weights = self.attention(query, key, value)
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(
+            attention, (batch_size, -1, self.embed_dim)
+        )
+        output = self.combine_heads(concat_attention)
+        return output
+
+
+class TransformerBlock(tf.keras.layers.Layer):
+    def __init__(self, embed_dim, num_heads, mlp_dim, dropout=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = MultiHeadSelfAttention(embed_dim, num_heads)
+        self.mlp = tf.keras.Sequential(
+            [
+                Dense(mlp_dim, activation=tfa.activations.gelu),
+                Dropout(dropout),
+                Dense(embed_dim),
+                Dropout(dropout),
+            ]
+        )
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.dropout1 = Dropout(dropout)
+        self.dropout2 = Dropout(dropout)
+
+    def call(self, inputs, training):
+        inputs_norm = self.layernorm1(inputs)
+        attn_output = self.att(inputs_norm)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = attn_output + inputs
+
+        out1_norm = self.layernorm2(out1)
+        mlp_output = self.mlp(out1_norm)
+        mlp_output = self.dropout2(mlp_output, training=training)
+        return mlp_output + out1
+
+
+class VisionTransformer(tf.keras.Model):
+    def __init__(
+        self,
+        image_size,
+        patch_size,
+        num_layers,
+        num_classes,
+        d_model,
+        num_heads,
+        mlp_dim,
+        channels=3,
+        dropout=0.1,
+    ):
+        super(VisionTransformer, self).__init__()
+        num_patches = (image_size // patch_size) ** 2
+        self.patch_dim = channels * patch_size ** 2
+
+        self.patch_size = patch_size
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.rescale = Rescaling(1.0 / 255)
+        self.pos_emb = self.add_weight(
+            "pos_emb", shape=(1, num_patches + 1, d_model)
+        )
+        self.class_emb = self.add_weight("class_emb", shape=(1, 1, d_model))
+        self.patch_proj = Dense(d_model)
+        self.enc_layers = [
+            TransformerBlock(d_model, num_heads, mlp_dim, dropout)
+            for _ in range(num_layers)
+        ]
+        self.mlp_head = tf.keras.Sequential(
+            [
+                LayerNormalization(epsilon=1e-6),
+                Dense(mlp_dim, activation=tfa.activations.gelu),
+                Dropout(dropout),
+                Dense(num_classes),
+            ]
+        )
+
+    def extract_patches(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patches = tf.reshape(patches, [batch_size, -1, self.patch_dim])
+        return patches
+
+    def call(self, x, training):
+        batch_size = tf.shape(x)[0]
+        x = self.rescale(x)
+        patches = self.extract_patches(x)
+        x = self.patch_proj(patches)
+
+        class_emb = tf.broadcast_to(
+            self.class_emb, [batch_size, 1, self.d_model]
+        )
+        x = tf.concat([class_emb, x], axis=1)
+        x = x + self.pos_emb
+
+        for layer in self.enc_layers:
+            x = layer(x, training)
+
+        # First (class token) is used for classification
+        x = self.mlp_head(x[:, 0])
+        return x
+
+config = tfds.download.DownloadConfig(register_checksums=True)
+dataset, dataset_info = tfds.load('oxford_flowers102', 
+                                  split=['train', 'validation', 'test'], 
+                                  as_supervised=True, 
+                                  with_info=True,
+                                  download_and_prepare_kwargs={"download_config": config})
+
+training_set, validation_set, test_set = dataset
+
+# TODO: Get the number of examples in each set from the dataset info.
+num_training_examples = dataset_info.splits['train'].num_examples
+num_validation_examples = dataset_info.splits['validation'].num_examples
+num_test_examples = dataset_info.splits['test'].num_examples
+
+print('There are {:,} images in the training set'.format(num_training_examples))
+print('There are {:,} images in the validation set'.format(num_validation_examples))
+print('There are {:,} images in the test set'.format(num_test_examples))
+
+# TODO: Get the number of classes in the dataset from the dataset info.
+num_classes = dataset_info.features['label'].num_classes
+print('There are {:,} classes in the dataset'.format(num_classes))
+
+
+batch_size = 32
+image_size = 256
+
+def format_image(image, label):
+    image = tf.cast(image, tf.float32)
+    image = tf.image.resize(image, (image_size, image_size))
+    # image = tf.reshape(image, [image_size, image_size, 3])
+    image /= 255
+    return image, label
+
+# training_set = training_set.map(tf_segment_label)
+# validation_set = validation_set.map(tf_segment_label)
+# testing_set = test_set.map(tf_segment_label)
+
+training_batches = training_set.shuffle(num_training_examples//4).map(format_image).batch(batch_size).prefetch(1)
+validation_batches = validation_set.map(format_image).batch(batch_size).prefetch(1)
+testing_batches = test_set.map(format_image).batch(batch_size).prefetch(1)
+
+training_batches
+
+v = vit.vit_l32(
+    image_size = 256,
+    activation='sigmoid',
+    pretrained=True,
+    include_top=True,
+    pretrained_top=False,
+    classes=num_classes,
+    # patch_size = 32,
+    # num_layers = 64,
+    # num_classes = num_classes,
+    # d_model = 64,
+    # num_heads = 16,
+    # mlp_dim = 128,
+    # channels= 3,
+    # dropout= 0.05,
+)
+histories = {}
+EPOCHS = 15
+
+v.compile(optimizer=keras.optimizers.Adam(1e-5),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+time_start = time()
+
+history = v.fit(training_batches,
+                    epochs=EPOCHS,
+                    validation_data=validation_batches)
+
+time_end = time() - time_start
+
+histories['transformers'] = history
+
+histories_saver(histories, "json/transformer_histories.json")
+histories = histories_loader("json/transformer_histories.json")
+
+# for model_name in histories.keys():
+#     print('Model: ', model_name)
+#     for history_object in ['loss', 'accuracy']:
+#         plot_history_object(histories, model_name, history_object, True)
+
+results = v.evaluate(testing_batches)
+
+f = open('outputs/transformers.txt', 'w')
+f.write('loss: ' + results[0])
+f.write('accuracy: ' + results[1])
+f.write('Training time: ' + time_end)
+f.close()
